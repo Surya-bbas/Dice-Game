25a8ef47b2ce084d0e73bb4958e5643f2ea95e2a
Surya-bbas <99864714+Surya-bbas@users.noreply.github.com>
2022-08-06 01:59:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
-let randomNumber1= Math.floor(Math.random() * 6) +1;
-let randomNumber2= Math.floor(Math.random() * 6) +1;
 
-let newImage1=`images/dice${randomNumber1}.png`
-let newImage2=`images/dice${randomNumber2}.png`
 
-document.querySelector(".img1").setAttribute("src",newImage1);
-document.querySelector(".img2").setAttribute("src",newImage2);
+document.querySelector("button").addEventListener("click",function(event){
 
-if (randomNumber1>randomNumber2){
+    console.log(event);
+    let randomNumber1= Math.floor(Math.random() * 6) +1;
+    let randomNumber2= Math.floor(Math.random() * 6) +1;
+
+
+
+  
+    document.querySelector(".img1").setAttribute("src",`images/dice${randomNumber1}.png`);
+    document.querySelector(".img2").setAttribute("src",`images/dice${randomNumber2}.png`);
+
+    if (randomNumber1>randomNumber2){
     document.querySelector("h1").innerHTML="Player 1 Won 🚩"
-}
-else if (randomNumber2>randomNumber1){
+    }
+    else if (randomNumber2>randomNumber1){
     document.querySelector("h1").innerHTML="Player 2 Won 🚩"
-}
-else if(randomNumber1===randomNumber2){
+    }
+    else if(randomNumber1===randomNumber2){
     document.querySelector("h1").innerHTML="Draw 🚩"
-}
+    }
 
-    
-
-
+})
 
